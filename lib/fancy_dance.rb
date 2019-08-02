14539b11b7d2534b9316183f104f4814859f53d6
Celal Cankaya <celalcankaya@outlook.com>
2019-08-02 15:53:22 +0000

    Automatically backed up by Learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,12 @@
+module FancyDance
+  module InstanceMethods              # you "include" these instance methods
+    def superjump 
+      "I'm just one jumping instance"
+    end
+  end
+  module ClassMethods                 # you "extend" these class methods
+    def megajump 
+      "I'm a whole class of MOFO's jumping"
+    end
+  end 
+end
