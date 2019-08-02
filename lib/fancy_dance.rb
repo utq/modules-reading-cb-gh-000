a51a5473485893b5e6b87aee7602ae20509b73a8
Celal Cankaya <celalcankaya@outlook.com>
2019-08-02 15:56:53 +0000

    Done.

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,29 @@
+module FancyDance
+  module InstanceMethods              # you "include" these instance methods
+    def superjump
+      "I'm just one jumping instance"
+    end
+    def twirl
+      "I'm twirling!"
+    end
+    def jump
+      "Look how high I'm jumping!"
+    end
+
+    def pirouette
+      "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+  end
+  module ClassMethods                 # you "extend" these class methods
+    def megajump
+      "I'm a whole class of MOFO's jumping"
+    end
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
+end
