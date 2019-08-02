1e043fdded636e81da21dfbc1deb0b7a791627e0
Celal Cankaya <celalcankaya@outlook.com>
2019-08-02 15:56:44 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -22,5 +22,8 @@ module FancyDance
     def megajump
       "I'm a whole class of MOFO's jumping"
     end
+    def metadata
+      "This class produces objects that love to dance."
+    end
   end
 end
